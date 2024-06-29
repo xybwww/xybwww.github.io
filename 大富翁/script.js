5be6223498a1002443bab1172b363e12fcae09aa
xybwww <160083773+xybwww@users.noreply.github.com>
2024-06-29 21:54:27 +0800

    0

--- 大富翁/script.js
+++ 大富翁/script.js
@@ -184,13 +184,14 @@ document.getElementById("dice").addEventListener("click", function () {
 
 //新的回合
 function newTurn() {
-  turn++;
-  if (turn === Object.keys(pawns).length) {
+  if (turn === Object.keys(pawns).length-1) {
     newDay();
   } else {
+    document.getElementById(Object.keys(pawns)[turn-1] + "card").className = "";
     document.getElementById(Object.keys(pawns)[turn] + "card").className = "turn";
     document.getElementById("diceDiv").style.display = "block";
     diceTimer = undefined;
+    turn++;
   }
 }
 
